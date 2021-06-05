0d4a0386c9db1f3f8cc1d0e3ff43a1a88da3de34
gaurangpandey <gaurang.pandey.gp@gmail.com>
2021-06-06 02:05:22 +0530

    commit 5

--- KataCalculator/src/main/java/com/gaurang/KataCalculator/StringCalculator.java
+++ KataCalculator/src/main/java/com/gaurang/KataCalculator/StringCalculator.java
@@ -3,15 +3,35 @@ package com.gaurang.KataCalculator;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-public class StringCalculator 
-{
+public class StringCalculator {
+	
+	private String delimiter , sum;
+	
+	private StringCalculator(String delimiter, String sum) {
+		this.delimiter=delimiter;
+		this.sum=sum;	
+	}
+	
+	
     public static int add(String input) {
     	if(input.isEmpty())
     		return 0;
     	
-    	Stream<String> sum=Arrays.stream(input.split(","));
-    	return sum.mapToInt(Integer::parseInt).sum();
+    	StringCalculator calculator;
+    	if(input.startsWith("//")) {
+    		String[] part=input.split("\n", 2);
+    		calculator = new StringCalculator(part[0].substring(2),part[1]);
     	}
+    	else
+    		calculator =new StringCalculator(",|\n",input);
+    	
+    	return calculator.number();
+    	}
+
+
+	private int number() {
+		return Arrays.stream(sum.split(delimiter)).mapToInt(Integer::parseInt).sum();
+	}
     	
 }
 
--- KataCalculator/src/test/java/com/gaurang/KataCalculator/StringCalculatorTest.java
+++ KataCalculator/src/test/java/com/gaurang/KataCalculator/StringCalculatorTest.java
@@ -20,5 +20,12 @@ public class StringCalculatorTest
     public void sumTwoNumberComma() {
     	assertThat(StringCalculator.add("2,2"), is(4));
     }
-    
+    @Test
+    public void sumNumberNewline() {
+    	assertThat(StringCalculator.add("1\n2,3"),is(6));
+    }
+    @Test
+    public void sumNumberDelimiter() {
+    	assertThat(StringCalculator.add("//;\n1;2"), is(3));
+    }
 }
